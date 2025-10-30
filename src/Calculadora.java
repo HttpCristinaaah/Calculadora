c02d1557b98acbcb2d4e4d2a42bd49489f17124b
Programador A <programadorA@example.com>
2025-10-30 02:01:59 +0100

    añadimos rama suma y añadimos add a calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,8 @@
 public class Calculadora {
-    // Método principal que llamará a las operaciones
     public double calcular(String op, double a, double b) {
         switch(op) {
-            case "suma": return new Suma().operar(a,b);
+            case "suma":
+            case "add": return new Suma().operar(a,b);
             case "resta": return new Resta().operar(a,b);
             case "multiplicacion": return new Multiplicacion().operar(a,b);
             case "division": return new Division().operar(a,b);
@@ -11,6 +11,6 @@ public class Calculadora {
     }
     public static void main(String[] args) {
         Calculadora c = new Calculadora();
-        System.out.println("2 + 3 = " + c.calcular("suma",2,3));
+        System.out.println("2 + 3 = " + c.calcular("add",2,3));
     }
 }
--- src/Suma.java
+++ src/Suma.java
@@ -1,5 +1,6 @@
 public class Suma {
     public double operar(double a, double b) {
+        System.out.println("[Suma] " + a + " + " + b);
         return a + b;
     }
 }
