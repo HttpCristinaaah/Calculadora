f9b8ecb1bd5cff31f9beb4f570c95ffa3c479d31
Programador A <programadorA@example.com>
2025-10-30 02:44:37 +0100

    hotfix prevenir division por 0

--- src/Division.java
+++ src/Division.java
@@ -1,5 +1,8 @@
 public class Division {
     public double operar(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir entre 0");
+        }
         return a / b;
     }
 }
